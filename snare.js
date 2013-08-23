4ec50004adc8a0d47434f264d2e57807ef2e5c14
newsomderek <newsom.derek@gmail.com>
2013-08-23 04:51:36 +0000

    Added ability to gather dimension to help with checking collisions

--- snare.js
+++ snare.js
@@ -6,6 +6,7 @@ snare.setup = function() {
         'enabled': false,
         'origin': {'x': 0, 'y': 0},
         'id': 'snare-rope',
+        'prey': 'snare-prey',
         'css': {
                 'position': 'absolute',
                 'top': '0px',
@@ -38,7 +39,37 @@ snare.setup = function() {
 
                    $('<div id="'+this.id+'" />').appendTo('body').css(this.css);
                }
+
+               this.check(x, y);
             }
+        },
+        'check': function(x, y) {
+            var that = this;
+
+            $('.'+this.prey).each(function() {
+
+                var dims = that.dimensions($('#'+that.id));
+                var preyDims = that.dimensions(this);
+
+            });
+        },
+        'dimensions': function(el) {
+            var pos = $(el).position();
+
+            var height = parseInt($(el).height());
+            var paddedHeight = height + parseInt($(el).css('padding-top')) +
+                parseInt($(el).css('padding-bottom'));
+
+            var width = parseInt($(el).width());
+            var paddedWidth = width + parseInt($(el).css('padding-left')) +
+                parseInt($(el).css('padding-right'));
+
+            return {
+                'x': parseInt(pos.left),
+                'y': parseInt(pos.top),
+                'width': width > paddedWidth ? width : paddedWidth,
+                'height': height > paddedHeight ? height : paddedHeight
+            };
         },
         'burn': function() {
             $('#'+this.id).remove();
